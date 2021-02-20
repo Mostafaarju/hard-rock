919072705796c628d7cbb4ca765972e7c7161c72
Raju Talukder <63764709+Mostafaarju@users.noreply.github.com>
2021-02-20 15:39:52 +0100

    add all file

---
+++ app.js
@@ -0,0 +1,72 @@
+document.getElementById("search-field").addEventListener("keypress", function (event) {
+    if (event.key === 'Enter') {
+        document.getElementById("search-button").click();
+    }
+});
+
+const searchSongs = async () => {
+    const searchText = document.getElementById('search-field').value;
+    const url = `https://api.lyrics.ovh/suggest/${searchText} `;
+    toggleSpinner();
+    const res = await fetch(url);
+    // Load Data
+    const data = await res.json();
+    displaySongs(data.data);
+}
+
+const displaySongs = songs => {
+    const songContainer = document.getElementById('song-container');
+    songContainer.innerHTML = '';
+    // Get Empty lyrics div 
+    const lyricsDiv = document.getElementById('song-lyrics');
+    lyricsDiv.innerText = '';
+    // 
+    songs.forEach(song => {
+        const songDiv = document.createElement('div');
+        songDiv.className = 'single-result row align-items-center my-3 p-3';
+        songDiv.innerHTML = `
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+            <audio controls src="${song.preview}" type="audio/mpeg"></audio>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button onclick="getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
+        </div>
+        `;
+        songContainer.appendChild(songDiv);
+        toggleSpinner();
+    });
+}
+
+const getLyric = async (artist, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayLyrics(data.lyrics);
+    } catch (error) {
+        displayError('Sorry! I Failed to load lyrics, Please try again later!!!');
+    }
+}
+const displayLyrics = lyrics => {
+    const lyricsDiv = document.getElementById('song-lyrics');
+    lyricsDiv.innerText = '';
+    lyricsDiv.innerText = lyrics;
+}
+
+const displayError = error => {
+    const errorTag = document.getElementById('error-message');
+    errorTag.innerText = error;
+}
+const toggleSpinner = (show) => {
+    const spinner = document.getElementById('loading-spinner');
+    const songs = document.getElementById('song-container');
+    // if (show) {
+    //     spinner.classList.remove('d-none');
+    // } else {
+    //     spinner.classList.add('d-none');
+    // }
+    spinner.classList.toggle('d-none');
+    songs.classList.toggle('d-none');
+}
